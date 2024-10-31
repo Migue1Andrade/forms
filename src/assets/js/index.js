10f7129a23c997d83275af09a9a524496e2d4077
Migue1Andrade <mfrancadeandrade@gmail.com>
2024-10-31 20:20:02 -0300

    update new features

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,7 +3,7 @@ const number = document.querySelector('.number');
 const email = document.querySelector('.email');
 const bornData = document.querySelector('.data');
 const cpf = document.querySelector('.cpf');
-const avenue = document.querySelector('.street');
+const adress = document.querySelector('.street');
 const cep = document.querySelector('.cep');
 const citty = document.querySelector('.citty');
 const state = document.querySelector('.state');
@@ -13,6 +13,7 @@ const hood = document.querySelector('.hood');
 const homeNumber = document.querySelector('.homeNumber');
 const complementsBox = document.querySelector('.complements-box') 
 const getInputs = document.querySelectorAll('input'); 
+const dataInput = document.querySelector('.data');
 
 const saveButton = document.getElementById('savedButton'); 
 const delButton = document.getElementById('delButton');
@@ -21,8 +22,12 @@ const regexNumber = /^\+?(\d{2}|\d{3})?\s?\(?\d{2}\)?\s?\d{4,5}\-?\d{4}$/;
 const regexFullName = /^[a-zA-Zà-ú ]{1,30}$/;
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const regexCpf = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
-const regexAvenue = /^[A-Za-zÀ-ú\s\d\.\-\']+$/;
+const regexAdress = /^[A-Za-zÀ-ú\s\d\.\-\']+$/;
 const cepRegex = /\d{5}-\d{3}/;
+
+dataInput.min = '1900-01-01';
+dataInput.max = '2024-12-31';
+dataInput.innerHTML.display = 'none';
 
 delButton.addEventListener('click', function(event) {
 	cleanInputs(); 
@@ -31,86 +36,91 @@ delButton.addEventListener('click', function(event) {
 enter.addEventListener('keypress', function(event) {
 	let keyCodeEvent = event.keyCode === 13;
 	if (keyCodeEvent) {
-		saveButton.click(); 
+		saveButton.click();
 	};
-}); 
+});
 
 const cleanInputs = () => {
-	const inputs = [fullName, number, email, bornData, cpf, avenue, citty, state, socialName, cep, hood, homeNumber, complementsBox];
+	const inputs = [fullName, number, email, bornData, cpf, adress, citty, state, socialName, cep, hood, homeNumber, complementsBox];
 
 	inputs.forEach(input => input.value = '');
 };
 
-const validateInputs = () => {
-	let validForm = true;
-
-	if (state.value.trim() === '') {
-		state.style.borderColor = 'red';
-		validForm = false;
-	};
+const isValidadeData = () => {
+	let isValidateForm = true;
 
 	if (fullName.value.trim() === '' || !regexFullName.test(fullName.value)) {
-		fullName.value = '';
-		fullName.style.borderColor = 'red';
-		validForm = false;
+		isValidateForm = false;
+		alert('preencha seu nome completo por favor');
+		fullName.focus();
+		return;	
 	};
 
 	if (number.value.trim() === '' || !regexNumber.test(number.value)) {
-		number.value = '';
-		number.style.borderColor = 'red'; 
-		validForm = false;
+		isValidateForm = false;
+		alert('preencha seu telefone por favor');
+		number.focus();
+		return;	
 	};
 
-	if (email.value.trim() === '' || !emailRegex.test(email.value)) {
-		email.value = '';
-		email.style.borderColor = 'red'; 
-		validForm = false;
-	};
-
-	if (bornData.value.trim() === '') {
-		bornData.value = '';
-		bornData.style.borderColor = 'red';
-		validForm = false;
-	};
-
-	if (citty.value.trim() === '') {
-		citty.style.borderColor = 'red';
-		validForm = false;
-	};
-
-	if (cep.value.trim() === '' || !cepRegex.test(cep.value)) {
-		cep.value = '';
-		cep.style.borderColor = 'red';
-		validForm = false;
+	if (bornData.value.trim() === ''|| bornData.value < '1900-01-01' || bornData.value > '2024-10-31' ) {
+		isValidateForm = false;
+		alert('preencha sua data de nascimento por favor');
+		bornData.focus();
+		return;	
 	};
 
 	if (cpf.value.trim() === '' || !regexCpf.test(cpf.value)) {
-		cpf.value = '';
-		cpf.style.borderColor = 'red';
-		validForm = false;
+		isValidateForm = false;
+		alert('preencha seu cpf por favor');
+		cpf.focus();
+		return;	
 	};
 
-	if (avenue.value.trim() === '' || !regexAvenue.test(avenue.value)) {
-		avenue.value = '';
-		avenue.style.borderColor = 'red';
-		validForm = false;
+	if (email.value.trim() === '' || !emailRegex.test(email.value)) {
+		isValidateForm = false;
+		alert('preencha seu email por favor');
+		email.focus();
+		return;	
 	};
-	
+
+	if (adress.value.trim() === '' || !regexAdress.test(adress.value)) {
+		isValidateForm = false;
+		alert('preencha seu endereco por favor');
+		adress.focus();
+		return;	
+	};
+
 	if (cep.value.trim() === '' || !cepRegex.test(cep.value)) {
-		cep.value = '';
-		cep.style.borderColor = 'red';
-		validForm = false;
+		isValidateForm = false;
+		alert('preencha seu CEP por favor');
+		cep.focus();
+		return;	
 	};
 
-	return validForm;
+	if (citty.value.trim() === '') {
+		isValidateForm = false;
+		alert('preencha sua cidade por favor');
+		citty.focus();
+		return;	
+	};
+
+	if (state.value.trim() === '') {
+		isValidateForm = false;
+		alert('preencha seu estado por favor');
+		state.focus();
+		return;		
+	};
+
+	return isValidateForm;
 };
 
 const localSave = () => {
-	const validForm = validateInputs();
+	const validForm = isValidadeData();
 
-	if (!validForm) {
-		alert('Preencha os campos obrigatórios!');
-	} else {
+	if (!validForm) return;
+
+	else {
 		const inputs = getInputs;
 		const formData = [];
 
@@ -148,15 +158,16 @@ const formatCpf = (input) => {
 	input.value = input.value.replace(/\D/g, '');
 
 	let i = 0;
+	
 	while (i < input.value.length) {
 		if (i === 3 || i === 7) {
-
 			input.value = input.value.slice(0, i) + '.' + input.value.slice(i);
 		};
+
 		if (i === 11) {
 			input.value = input.value.slice(0, i) + '-' + input.value.slice(i);
-
 		};
+
 		i++;
 	};
 
@@ -191,19 +202,20 @@ const onlyLetters = input => {
 
 const onlyNumbers = input => {
 	input.value = input.value.replace(/\D/g, '');
-	if (input.value.length > 5) {
+	if (input.value.length > 4) {
 		input.value = input.value.slice(0, 5);
 	};
 };
 
 const limiteOfChars = input => {
-	if (input.value.length > 20) {
-		input.value = input.value.slice(0, 20);
+	if (input.value.length > 70) {
+		input.value = input.value.slice(0, 60);
 	};
 };
 
-const limiteOfCharsComplementsBox = input => {
-	if (input.value.length > 150) {
-		input.value = input.value.slice(0, 150);
+const limiteOfCharsName = input => {
+	input.value = input.value.replace(/[^a-zA-Z ]/g, '');
+	if (input.value.length > 40) {
+		input.value = input.value.slice(0, 40);
 	};
 };
